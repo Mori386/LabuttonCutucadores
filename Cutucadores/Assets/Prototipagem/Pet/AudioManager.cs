9a1b3409d5813b1992a0e8b2b156c054121f1477
Rodrigo Morizono <rkmorizono@gmail.com>
2023-09-19 09:34:22 -0300

    Update AudioManager.cs

--- Cutucadores/Assets/Prototipagem/Pet/AudioManager.cs
+++ Cutucadores/Assets/Prototipagem/Pet/AudioManager.cs
@@ -6,47 +6,52 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
     public Sound[] sounds; // Um array de aúdio, isso permite utilizar uma boa quantidade de aúdio através de uma única função
+    public enum SoundType
+    {
+        Drill,
+        DrillHit,
 
+    }
     //public static AudioManager instance;
     //internal readonly object currentlyPlaying;
 
     void Awake()
     {
-        //Caso haja bug de duplicidade de audiomanager, descomentar as linhas abaixo.
-
-        /* if (instance != null)
-         {
-             Destroy(gameObject);
-             return;
-         }
-
-         instance = this;
-
-         DontDestroyOnLoad(gameObject);*/
+         Instance = this;
 
         foreach (Sound s in sounds) // Foreach é para percorrer as posições de memória do array. podemos dizer que a Letra S são os indices
         {
             s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
 
+            s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
-
-
         }
     }
-
-    public void Play(string name) // Caso o nome do aúdio ñ seja encontrado, vai dar um super hiper mega bug. Se atentar ao nome pré-definido do Som
+    public Sound GetSound(SoundType soundType)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (sounds[(int)soundType] != null)
+        {
+            return sounds[(int)soundType];
+        }
+        else
+        {
+            Debug.LogError("Sound file not found, Integer "+ (int)soundType+" doesnt exist");
+            return null;
+        }
+    }
+    public void Play(SoundType soundType) // Caso o nome do aúdio ñ seja encontrado, vai dar um super hiper mega bug. Se atentar ao nome pré-definido do Som
+    {
+        Sound s = GetSound(soundType);
         s.source.Play();
     }
 
-    public void Stop(string name)
+    public void Stop(SoundType soundType)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = GetSound(soundType);
         if (s == null)
         {
             Debug.LogWarning("Sound: " + name + " not found!");
