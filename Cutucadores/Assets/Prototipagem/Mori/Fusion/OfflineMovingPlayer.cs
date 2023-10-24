ac47d0a1208adeb7e9f1c9cb4a7aec047b8e65f7
Rodrigo Morizono <rkmorizono@gmail.com>
2023-10-24 12:25:46 -0300

    OfflineTestPlayer

--- Cutucadores/Assets/Prototipagem/Mori/Fusion/OfflineMovingPlayer.cs
+++ Cutucadores/Assets/Prototipagem/Mori/Fusion/OfflineMovingPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class OfflineMovingPlayer : MonoBehaviour
 {
@@ -20,13 +21,18 @@ public class OfflineMovingPlayer : MonoBehaviour
     private void FixedUpdate()
     {
         
+        Move(movementInput.y);
+        Rotate(movementInput.x);
     }
     private void Move(float YDirection)
     {
+        float deltaTime = Time.deltaTime;
+        rb.velocity = transform.forward * YDirection*10;
+
 
     }
     private void Rotate(float XDirection)
     {
-
+        transform.Rotate(0, XDirection * Time.deltaTime * characterData.rotationSpeed*10, 0);
     }
 }
