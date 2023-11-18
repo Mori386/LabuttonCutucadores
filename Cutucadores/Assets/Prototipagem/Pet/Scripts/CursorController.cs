66e715ba9846788d2419f1bba90407f5df9bb562
Petkovic Dos Santos <89022214+Petinho29@users.noreply.github.com>
2023-11-18 11:54:58 -0300

    Fix bug controller 2

--- Cutucadores/Assets/Prototipagem/Pet/Scripts/CursorController.cs
+++ Cutucadores/Assets/Prototipagem/Pet/Scripts/CursorController.cs
@@ -5,97 +5,122 @@ using UnityEngine.UI;
 
 public class CursorController : MonoBehaviour
 {
-    public GameObject cursorObject, cursorObject2, config, inicialMenu, roles, roleClientHost, Blueprint, credits;
-    public GameObject[] Okays;
+    [Header("CURSOR")]
+    public GameObject cursorObject;
+    public GameObject cursorObject2;
+    public GameObject mira;
     public Camera mainCamera;
+
+    [Header("OBJETOS DO LIVRO")]
+    public GameObject config;
+    public GameObject inicialMenu;
+    public GameObject roles;
+    public GameObject roleClientHost;
+    public GameObject Blueprint;
+    public GameObject credits;
     public Transform targetObject, targetObject2, targetObject3, Book; // O objeto 3D para onde o cursor 3D será movido
     public Animator animHand, animBook, animPage; // animações da mão e caderno
-    //public Image  // blueprint seleção
 
+    [Header("OBJETOS DO BLUEPRINT")]
+    public GameObject[] Okays;
+    public GameObject tanques;
 
-    private bool isMoving = false;
-    private bool isChange = false;
-
+    [Header("PARA ANIMAÇÃO")]
     public float moveTime = 2f; // Tempo total (tempo que o cursor fica na posição de destino + tempo de deslocamento)
     public float pauseTime = 1.5f; // Tempo que o cursor fica na posição de destino
-    public float smoothTime = 0.5f; 
+    public float smoothTime = 0.5f;
 
     private float moveDuration; // Tempo de deslocamento
     private float timer = 0.0f; // verificação de tempo max
 
+    [Header("BOOLEANOS")]
+    private bool isMoving = false;
+    private bool isChange = false;
+    private bool isReturn = false;
+
+    [Header("VETORES")]
     private Vector3 startPosition;
     private Vector3 targetPosition;
-
     public Vector3 mousePosition;
 
     void Start()
     {
         mainCamera = Camera.main;
 
-        Cursor.visible = true; // Esconde o cursor do mouse
+        Cursor.visible = false; // Esconde o cursor do mouse
         Cursor.lockState = CursorLockMode.Confined; // Mantém o cursor dentro da janela do jogo.
 
         startPosition = cursorObject.transform.position; //para retorno da posição inicial
         moveDuration = moveTime - pauseTime; // valor do tempo de deslocamento
-                                            
+
         StartCoroutine(MoveBookSmoothly());
+        cursorObject2.SetActive(false);
     }
 
     void Update()
     {
+        Cursor.lockState = CursorLockMode.Confined;
+
         if (!isMoving)
         {
             // Atualiza a posição do cursor 3D para seguir o mouse
             mousePosition = Input.mousePosition;
-            mousePosition.z = 3f; // Distância do cursor em relação à câmera
+            mousePosition.z = 2.7f; // Distância do cursor em relação à câmera
             cursorObject.transform.position = mainCamera.ScreenToWorldPoint(mousePosition);
+            mira.transform.position = mainCamera.ScreenToWorldPoint(mousePosition);
+
+            if (isChange == true) // else buga a animação
+            {
+                // atualiza a posição de cursorObject2 para seguir o mouse
+                cursorObject2.SetActive(true);
+                mousePosition.z = 1f;
+                cursorObject2.transform.position = mainCamera.ScreenToWorldPoint(mousePosition);
+            }
+
+            if (isReturn == true)
+            {
+                cursorObject.SetActive(true);
+                cursorObject2.SetActive(false);
+                mousePosition.z = 2.7f;
+            }
 
         }
         else
         {
-            // O objeto 3D está se movendo para a posição de destino para realizar a animação
+            // Lógica de animação suave
             timer += Time.deltaTime;
 
+            // Interpola suavemente a posição do cursor 3D durante o tempo de deslocamento
             if (timer < moveDuration)
             {
-                // Interpole suavemente a posição do cursor 3D durante o tempo de deslocamento
                 float t = timer / moveDuration;
                 cursorObject.transform.position = Vector3.Lerp(startPosition, targetPosition, t);
                 cursorObject2.transform.position = Vector3.Lerp(startPosition, targetPosition, t);
             }
+
+            // O tempo de deslocamento acabou; o cursor fica parado
             else if (timer >= moveDuration && timer < moveTime)
             {
-                // O tempo de deslocamento acabou; o cursor fica parado
                 cursorObject.transform.position = targetPosition;
                 cursorObject2.transform.position = targetPosition;
-
             }
             else
             {
-                // O tempo de espera acabou, animação executada, retornar o objeto 3D para a posição do mouse --- calculo realizado no LockMousePosition
-                //cursorObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 isMoving = false;
             }
         }
 
-        if (isChange == true)
+        if (isChange == true) // else buga a animação
         {
-            ChangeCursorObject(cursorObject2, 10f);
+            //ChangeCursorObject(cursorObject2, 10f);
             cursorObject2.SetActive(true);
-            mousePosition.z = 1f;
-            cursorObject2.transform.position = mainCamera.ScreenToWorldPoint(mousePosition);
+            cursorObject.SetActive(false);
         }
-        else
-        {
-            ChangeCursorObject(cursorObject, 10f);
-            cursorObject2.SetActive(false);
-            mousePosition.z = 3f;
-        }
-
 
     }
 
-    public void NextPage(bool kitten) // chamando esse void através dos botões em cena, caso o parametro do metodo seja verdadeiro, a sequência do update é executada
+    // todos os voids abaixo são chamados através de botões na cena, de acordo com os seus respectivos nomes
+    public void NextPage(bool kitten)  // passar pag
     {
         // Inicializa a posição de início e destino
         startPosition = cursorObject.transform.position;
@@ -106,7 +131,7 @@ public class CursorController : MonoBehaviour
         timer = 0.0f;
     }
 
-    public void Credits(bool kitten)
+    public void Credits(bool kitten) // menu creditos
     {
         startPosition = cursorObject.transform.position;
         targetPosition = targetObject.position;
@@ -116,10 +141,20 @@ public class CursorController : MonoBehaviour
         timer = 0.0f;
     }
 
+    public void ReturnCredits(bool kitten) // sair do menu de creditos
+    {
+        startPosition = cursorObject.transform.position;
+        targetPosition = targetObject.position;
+
+        isMoving = true;
+        StartCoroutine(DelayReturnCredits());
+        timer = 0.0f;
+    }
+
     public void ReturnPage(bool kitten) // anim para retorno
     {
-       startPosition = targetObject.transform.position;
-       targetPosition = targetObject.position;
+        startPosition = cursorObject.transform.position;
+        targetPosition = targetObject.position;
 
         isMoving = true;
         StartCoroutine(DelayPreviousPage());
@@ -139,7 +174,7 @@ public class CursorController : MonoBehaviour
 
     public void OpenBook(bool kitten) // anim para abrir livro
     {
-        startPosition = cursorObject.transform.position;
+        startPosition = targetObject.transform.position;
         targetPosition = targetObject.position;
 
         isMoving = true;
@@ -150,41 +185,34 @@ public class CursorController : MonoBehaviour
     public void BlueprintSelect() // anim para blueprint de seleção
     {
         Blueprint.SetActive(true);
+        tanques.SetActive(true);
+        roleClientHost.SetActive(false);
 
         // Movimentar blueprint para perto da camera.
         Vector3 posicaoAtual = Blueprint.transform.localPosition;
         posicaoAtual.z = -183f; // posição do obj
 
         Blueprint.transform.localPosition = posicaoAtual;
-
         isChange = true;
+        isReturn = false;
     }
 
-    void ChangeCursorObject(GameObject newCursorObject, float newMousePositionZ)
+    public void ReturnBlueprintSelect() // sair do blue de seleção
     {
-        Debug.Log("change");
-        // Desativa o cursor anterior (se houver um)
-        if (cursorObject != null)
-        {
-            cursorObject.SetActive(false);
-        }
-
-        // Atualiza a posição z do mouse
-        mousePosition.z = newMousePositionZ;
-
-        // Ativa o novo cursor
-        newCursorObject.SetActive(true);
-
-        // Atualiza a referência para o novo objeto de cursor
-        cursorObject = newCursorObject;
+        Blueprint.SetActive(false);
+        tanques.SetActive(false);
+        roleClientHost.SetActive(true);
+        isReturn = true;
+        isChange = false;
     }
 
     public void OkPlayers(int playerNumber) // confirmação dos players
     {
 
-        switch (playerNumber) // vinculado a cada highligthed
+        switch (playerNumber) // vinculado a cada highligthed em cena
         {
             case 0:
+                Debug.Log("Anim roda");
                 startPosition = cursorObject2.transform.position;
                 targetPosition = Okays[0].transform.position;
                 isMoving = true;
@@ -224,7 +252,7 @@ public class CursorController : MonoBehaviour
 
     }
 
-    public void SetActiveWithDelay(GameObject go, bool state, float delay)
+    public void SetActiveWithDelay(GameObject go, bool state, float delay) // delay global
     {
         StartCoroutine(SetActiveDelayCoroutine(go, state, delay));
     }
@@ -233,6 +261,7 @@ public class CursorController : MonoBehaviour
         yield return new WaitForSeconds(delay);
         go.SetActive(state);
     }
+
     // Delay animações
     IEnumerator DelayNextPage()
     {
@@ -248,7 +277,7 @@ public class CursorController : MonoBehaviour
         yield return new WaitForSeconds(0.10f);
         config.SetActive(true);
 
-        StartCoroutine(LockMousePosition(targetObject, moveTime));
+        StartCoroutine(LockMousePosition(cursorObject.transform, moveTime));
     }
     IEnumerator DelayCredits()
     {
@@ -264,29 +293,42 @@ public class CursorController : MonoBehaviour
         yield return new WaitForSeconds(0.10f);
         credits.SetActive(true);
 
-        StartCoroutine(LockMousePosition(targetObject, moveTime));
+        StartCoroutine(LockMousePosition(cursorObject.transform, moveTime));
     }
 
+    IEnumerator DelayReturnCredits()
+    {
+        animHand.Play("arm_AMT|VirarPag 0");
+
+        yield return new WaitForSeconds(0.15f);
+        animPage.Play("FolhaVirando 0 0");
+
+        yield return new WaitForSeconds(0.15f);
+        config.SetActive(true);
+
+        yield return new WaitForSeconds(0.10f);
+        credits.SetActive(false);
+
+        StartCoroutine(LockMousePosition(cursorObject.transform, moveTime));
+    }
 
     IEnumerator DelayPreviousPage()
     {
-        
         animHand.Play("arm_AMT|VirarPag 0");
 
         yield return new WaitForSeconds(0.15f);
         animPage.Play("FolhaVirando 0");
 
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(0.15f);
         config.SetActive(false);
 
         yield return new WaitForSeconds(0.10f);
         inicialMenu.SetActive(true);
-        StartCoroutine(LockMousePosition(targetObject, moveTime));
+        StartCoroutine(LockMousePosition(cursorObject.transform, moveTime));
     }
 
     IEnumerator DelayCloseBook()
     {
-
         animHand.Play("arm_AMT|VirarPag 0");
 
         yield return new WaitForSeconds(0.15f);
@@ -299,8 +341,7 @@ public class CursorController : MonoBehaviour
         yield return new WaitForSeconds(0.25f);
         roleClientHost.SetActive(true);
 
-        StartCoroutine(LockMousePosition(targetObject, moveTime));
-
+        StartCoroutine(LockMousePosition(cursorObject.transform, moveTime));
     }
 
     IEnumerator DelayOpenBook()
@@ -309,21 +350,18 @@ public class CursorController : MonoBehaviour
 
         yield return new WaitForSeconds(0.35f);
         animBook.Play("CadernoFechando 0");
-        
 
         yield return new WaitForSeconds(0.50f);
         roleClientHost.SetActive(false);
-        //animHand.Play("CadernoFechando 0");
-
 
         yield return new WaitForSeconds(0.25f);
         roles.SetActive(true);
         inicialMenu.SetActive(true);
-        StartCoroutine(LockMousePosition(targetObject, moveTime));
+        StartCoroutine(LockMousePosition(cursorObject.transform, moveTime));
 
     }
 
-    IEnumerator MoveBookSmoothly()
+    IEnumerator MoveBookSmoothly() 
     {
         Vector3 pontoOrigem = Book.position;
         float duration = 1.0f; // Tempo total da transição em segundos
@@ -333,7 +371,7 @@ public class CursorController : MonoBehaviour
             Vector3 pontoAtual = Vector3.Lerp(pontoOrigem, targetObject3.position, t);
             pontoAtual.y = pontoOrigem.y + t * t * (targetObject3.position.y - pontoOrigem.y);
 
-            Debug.Log("Ponto atual: " + pontoAtual);
+            //Debug.Log("Ponto atual: " + pontoAtual);
 
             // Movimenta o objeto para o ponto atual
             Book.position = pontoAtual;
@@ -349,7 +387,6 @@ public class CursorController : MonoBehaviour
 
         yield return new WaitForSeconds(0.45f);
         inicialMenu.SetActive(true);
-
 
     }
 
