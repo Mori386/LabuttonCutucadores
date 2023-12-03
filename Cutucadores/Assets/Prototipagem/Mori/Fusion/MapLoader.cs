c7d776573aadd8577c098dfe2338232aa6529e12
Rodrigo Morizono <rkmorizono@gmail.com>
2023-12-03 17:06:14 -0300

    Update MapLoader.cs

--- Cutucadores/Assets/Prototipagem/Mori/Fusion/MapLoader.cs
+++ Cutucadores/Assets/Prototipagem/Mori/Fusion/MapLoader.cs
@@ -19,16 +19,17 @@ public class MapLoader : NetworkSceneManagerBase
     }
     public static IEnumerator Load(string sceneName, int mapInt)
     {
-        Scene oldScene = SceneManager.GetActiveScene();
+        //Scene oldScene = SceneManager.GetActiveScene();
 
-        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-        while (!ao.isDone && ao != null)
-        {
-            yield return null;
-        }
+        //AsyncOperation ao = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        //while (!ao.isDone && ao != null)
+        //{
+        //    yield return null;
+        //}
         //SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
 
         Instance.mapIndex = mapInt;
+        yield return null;
         Instance.Runner.SetActiveScene(sceneName);
     }
     protected override IEnumerator SwitchScene(SceneRef prevScene, SceneRef newScene, FinishedLoadingDelegate finished)
@@ -36,7 +37,8 @@ public class MapLoader : NetworkSceneManagerBase
         if (prevScene != SceneRef.None)
         {
             List<NetworkObject> sceneObjects = new List<NetworkObject>();
-            yield return SceneManager.LoadSceneAsync(mapIndex, LoadSceneMode.Single);
+            yield return SceneManager.LoadSceneAsync(mapIndex, LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync(0);
             sceneObjects = FindNetworkObjects(SceneManager.GetActiveScene(), disable: false);
 
             yield return null;
