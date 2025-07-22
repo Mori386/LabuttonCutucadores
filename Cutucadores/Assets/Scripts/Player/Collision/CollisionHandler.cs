4bea8d35a5a188fa4330722c1c2a5a7701a55ce6
arii-mms <129689424+arii-mms@users.noreply.github.com>
2025-07-22 18:19:06 -0300

    Fix: UI da win screen e pontuação
    - Levels 2 e 3 estavam sem EventSystem e por isso botões não funcionavam
    - Agora bater em players durante a invencibilidade não concede pontos

--- Cutucadores/Assets/Scripts/Player/Collision/CollisionHandler.cs
+++ Cutucadores/Assets/Scripts/Player/Collision/CollisionHandler.cs
@@ -26,7 +26,7 @@ public class CollisionHandler : NetworkBehaviour
                         //Contact with other player body with this player drill
                         if (Object.HasStateAuthority)
                         {
-                            if (!collision.transform.root.GetComponent<HPHandler>().HasShield) //se o shield do outro já estiver desativado, ganha ponto
+                            if (!collision.transform.root.GetComponent<HPHandler>().HasShield && !collision.transform.root.GetComponent<HPHandler>().IsInvulnerable) //se o shield do outro já estiver desativado, ganha ponto
                                 transform.root.GetComponent<HPHandler>().IncreaseScore(1);
                             networkCharacterController.Knockback(collision.GetContact(0).point, true);
                             StartFallChecker(collision);
